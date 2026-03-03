4692a652c77cbf97d6fc4bea948f30fd072f9d22
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-03-03 17:03:46 -0300

    feat: Adiciona Menu Principale do Jogo | Verificar Novas partes

---
+++ index.js
@@ -0,0 +1,18 @@
+op = Number(prompt("Insira a Opção que Gostaria de Fazer:  "))
+
+
+function menu(){
+    switch(op){
+         case 1:
+            jogar();
+        break;
+        case 2:
+            lutar();
+            break;
+        case 3:
+            menu();
+            break;
+    }
+}
+
+menu();
