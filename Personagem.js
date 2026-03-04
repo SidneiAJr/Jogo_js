7bd8dd53362b76be69432aa90152c4bcb64cc469
Sid Jr <sidalbertok@gmail.com>
2026-03-04 14:34:18 -0300

    Update Personagem.js

--- Personagem.js
+++ Personagem.js
@@ -1,10 +1,10 @@
 class Personagem{
-    constructor(nomePersonagem, Classe, Dano, Mana, Exp, Reliquia,Vida){
+    constructor(nomePersonagem, Classe, Dano, Mana, Level, Reliquia,Vida){
         this.nomePersonagem = nomePersonagem;
         this.Classe = Classe;
         this.Dano = Dano;
         this.Mana = Mana;
-        this.Exp = Exp;
+        this.Level = Level;
         this.Reliquia = Reliquia;
         this.Vida = Vida;
     }
