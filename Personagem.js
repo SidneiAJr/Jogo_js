8e08257ba39005bacf095da03b8ab669933c0792
JianLPalini <jianpalini11@gmail.com>
2026-03-04 14:25:33 -0300

    feat: Adiciona Personagem

---
+++ Personagem.js
@@ -0,0 +1,10 @@
+class Personagem{
+    constructor(nomePersonagem, Classe, Dano, Mana, Exp, Reliquia){
+        this.nomePersonagem = nomePersonagem;
+        this.Classe = Classe;
+        this.Dano = Dano;
+        this.Mana = Mana;
+        this.Exp = Exp;
+        this.Reliquia = Reliquia;
+    }
+}
